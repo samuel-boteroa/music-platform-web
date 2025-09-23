53ae1bd9f3151169c9e312a5d3cccab0bdb841cd
Samuel <samuelboteroalzate@gmail.com>
2025-09-23 09:48:09 -0500

    Revert "Update MusicDataService.cs"
    This reverts commit c7a1244360f5af06163a4c29b76d472847a7433b.

--- MusicPlatformWeb/Services/MusicDataService.cs
+++ MusicPlatformWeb/Services/MusicDataService.cs
@@ -1,40 +1,40 @@
-using MusicClasses;
+﻿using MusicClasses;
 using MusicClasses.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MusicPlatformWeb.Services
 {
     public class MusicDataService : IMusicDataService
     {
-        private readonly Dictionary<int, List<Playlist>> programPlaylists;
+        private readonly Dictionary<Guid, List<Playlist>> programPlaylists;
 
         public MusicDataService()
         {
-            programPlaylists = new Dictionary<int, List<Playlist>>();
+            var dummyId = new Guid("11223344-5566-7788-99aa-bbccddeeff00");
 
-            // Usuario ficticio con ID = 1
-            programPlaylists[1] = new List<Playlist>
+            var playlists = new List<Playlist>
             {
-                new Playlist { Name = "Suavemente", Description = "Clásicos de merengue" },
-                new Playlist { Name = "Esquelefacina", Description = "Lo mejor del perreo" }
+                new Playlist("Suavemente"),
+                new Playlist("Esquelefacina"),
+                new Playlist("Perreo")
+            };
+
+            programPlaylists = new Dictionary<Guid, List<Playlist>>
+            {
+                { dummyId, playlists }
             };
         }
 
-        public async Task<List<Playlist>> GetUserPlaylistsAsync(int userId)
+        // Método asíncrono con PascalCase
+        public async Task<List<Playlist>> GetUserPlaylistsAsync(User user)
         {
-            if (programPlaylists.TryGetValue(userId, out var playlists))
+            if (programPlaylists.TryGetValue(user.id, out var playlists))
+            {
                 return await Task.FromResult(playlists);
+            }
 
-            return new List<Playlist>();
-        }
-
-        public async Task AddPlaylistAsync(int userId, Playlist newPlaylist)
-        {
-            if (!programPlaylists.ContainsKey(userId))
-                programPlaylists[userId] = new List<Playlist>();
-
-            programPlaylists[userId].Add(newPlaylist);
-            await Task.CompletedTask;
+            return await Task.FromResult(new List<Playlist>());
         }
     }
 }
-
